7fefdce86bcc83fdb593019135c279df582e3357
Hans DL5DI <dl5di@users.noreply.github.com>
2019-03-27 23:06:33 +0100

    Update cdmrmmdvmprotocol.cpp
    Create different DSTAR module IDs for different incoming DMR-IDs to prevent overlapping for repeaters which use the same callsign for different DSTAR, DMR, YSF etc. services.
    The module ID is created from the last digit of the DMR-ID, starting with charcter "H" to reserve A-G for real DSTAR repeater modules.
    The way of calculation makes sure that the ID will always be the same for a specific service. 
    There is a minor risk that a repeater uses different DMR-IDs with the same final digit.
    It is just a simple and quick hack with a single line of code.

--- src/cdmrmmdvmprotocol.cpp
+++ src/cdmrmmdvmprotocol.cpp
@@ -511,7 +511,7 @@ bool CDmrmmdvmProtocol::IsValidKeepAlivePacket(const CBuffer &Buffer, CCallsign 
     {
         uint32 uiRptrId = MAKEDWORD(MAKEWORD(Buffer.data()[10],Buffer.data()[9]),MAKEWORD(Buffer.data()[8],Buffer.data()[7]));
         callsign->SetDmrid(uiRptrId, true);
-        callsign->SetModule(MMDVM_MODULE_ID);
+        callsign->SetModule(char(72 + (uiRptrId % 10)));
         valid = callsign->IsValid();
     }
     return valid;
@@ -526,7 +526,7 @@ bool CDmrmmdvmProtocol::IsValidConnectPacket(const CBuffer &Buffer, CCallsign *c
     {
         uint32 uiRptrId = MAKEDWORD(MAKEWORD(Buffer.data()[7],Buffer.data()[6]),MAKEWORD(Buffer.data()[5],Buffer.data()[4]));
         callsign->SetDmrid(uiRptrId, true);
-        callsign->SetModule(MMDVM_MODULE_ID);
+        callsign->SetModule(char(72 + (uiRptrId % 10)));
         valid = callsign->IsValid();
         if ( !valid)
         {
@@ -545,7 +545,7 @@ bool CDmrmmdvmProtocol::IsValidAuthenticationPacket(const CBuffer &Buffer, CCall
     {
         uint32 uiRptrId = MAKEDWORD(MAKEWORD(Buffer.data()[7],Buffer.data()[6]),MAKEWORD(Buffer.data()[5],Buffer.data()[4]));
         callsign->SetDmrid(uiRptrId, true);
-        callsign->SetModule(MMDVM_MODULE_ID);
+        callsign->SetModule(char(72 + (uiRptrId % 10)));
         valid = callsign->IsValid();
         if ( !valid)
         {
@@ -565,7 +565,7 @@ bool CDmrmmdvmProtocol::IsValidDisconnectPacket(const CBuffer &Buffer, CCallsign
     {
         uint32 uiRptrId = MAKEDWORD(MAKEWORD(Buffer.data()[7],Buffer.data()[6]),MAKEWORD(Buffer.data()[5],Buffer.data()[4]));
         callsign->SetDmrid(uiRptrId, true);
-        callsign->SetModule(MMDVM_MODULE_ID);
+        callsign->SetModule(char(72 + (uiRptrId % 10)));
         valid = callsign->IsValid();
     }
     return valid;
@@ -580,7 +580,7 @@ bool CDmrmmdvmProtocol::IsValidConfigPacket(const CBuffer &Buffer, CCallsign *ca
     {
         uint32 uiRptrId = MAKEDWORD(MAKEWORD(Buffer.data()[7],Buffer.data()[6]),MAKEWORD(Buffer.data()[5],Buffer.data()[4]));
         callsign->SetDmrid(uiRptrId, true);
-        callsign->SetModule(MMDVM_MODULE_ID);
+        callsign->SetModule(char(72 + (uiRptrId % 10)));
         valid = callsign->IsValid();
         if ( !valid)
         {
@@ -600,7 +600,7 @@ bool CDmrmmdvmProtocol::IsValidOptionPacket(const CBuffer &Buffer, CCallsign *ca
     {
         uint32 uiRptrId = MAKEDWORD(MAKEWORD(Buffer.data()[7],Buffer.data()[6]),MAKEWORD(Buffer.data()[5],Buffer.data()[4]));
         callsign->SetDmrid(uiRptrId, true);
-        callsign->SetModule(MMDVM_MODULE_ID);
+        callsign->SetModule(char(72 + (uiRptrId % 10)));
         valid = callsign->IsValid();
     }
     return valid;
@@ -683,7 +683,7 @@ bool CDmrmmdvmProtocol::IsValidDvHeaderPacket(const CBuffer &Buffer, CDvHeaderPa
                 // build DVHeader
                 CCallsign csMY = CCallsign("", uiSrcId);
                 CCallsign rpt1 = CCallsign("", uiRptrId);
-                rpt1.SetModule(MMDVM_MODULE_ID);
+                rpt1.SetModule(char(72 + (uiRptrId % 10)));
                 CCallsign rpt2 = m_ReflectorCallsign;
                 rpt2.SetModule(DmrDstIdToModule(uiDstId));
                 
